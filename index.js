57fc80e6dbfa818661fa5b06f1c2d22bdb4edc84
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-04 22:14:51 +0300

    added markup

---
+++ index.js
@@ -0,0 +1,14 @@
+const blogTitle = document.getElementById("blog-title")
+const blogForm = document.getElementById("blog-form")
+const blogContent = document.getElementById("blog-content")
+const submitBtn = document.getElementById("submit-btn")
+
+blogForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const post = {
+        title: blogTitle.value,
+        content: blogContent.value
+    }
+
+    console.log(post)
+})
