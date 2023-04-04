ab8e1ce6039cc726b33324319bd015462cd1fa78
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-04 23:02:27 +0300

    updated script

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const blogForm = document.getElementById("blog-form")
 const blogContent = document.getElementById("blog-content")
 const submitBtn = document.getElementById("submit-btn")
 
+const responseMessage = document.getElementById("response-message")
+const responseTitle = document.getElementById("response-title")
+const responseId = document.getElementById("response-id") 
+const responseContent = document.getElementById("response-content")
+
 const url = 'https://us-central1-open-classrooms-js-for-the-web.cloudfunctions.net/widgets';
 
 blogForm.addEventListener("submit", (event) => {
@@ -38,6 +43,6 @@ async function submitFormData(post) {
         const requestPromise = makeRequest(post)
         const response = await requestPromise
     } catch (errorResponse) {
-        
+
     }
 }
